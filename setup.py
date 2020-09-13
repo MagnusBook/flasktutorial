35298d800f27208cddf165b40b119f750cb08648
Magnus Book <magnus@bityoga.com>
2020-09-13 14:58:38 +0200

    Hello world

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='flasktutorial',
+    packages=['flasktutorial'],
+    include_package_date=True,
+    install_requires=[
+        'flask'
+    ],
+)
