9cb4f0eb348214577ae9534a000c0f34d619bf1e
Magnus Book <magnus@bityoga.com>
2020-09-14 15:54:19 +0200

    Simple database implemented

---
+++ flasktutorial/views.py
@@ -0,0 +1,15 @@
+from flask import render_template
+from flasktutorial import app, query_db
+
+
+@app.route('/')
+def index():
+    articles = query_db('SELECT rowid, * FROM article;')
+    return render_template('index.html', articles=articles)
+
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+    article = query_db(
+        'SELECT * FROM article WHERE id=?;', (str(article_id)), True)
+    return render_template('article.html', article=article)
