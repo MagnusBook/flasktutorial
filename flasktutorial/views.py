30c7a0ee35d36c69bbec4738b37ad287997d8244
Magnus Book <magnus@bityoga.com>
2020-09-14 12:37:24 +0200

    Templating with placeholder data

--- flasktutorial/views.py
+++ flasktutorial/views.py
@@ -1,5 +1,17 @@
+import datetime
 from flasktutorial import app
+from flask import render_template
+
+articles = [{'id': 1, 'title': 'Something bad happened today!', 'text': 'Today was a bad day',
+             'date': datetime.date(1999, 4, 12)}, {'id': 2, 'title': 'Something good happened today!', 'text': 'Today was a good day',
+                                                   'date': datetime.date(2004, 6, 1)}]
+
 
 @app.route('/')
 def index():
-    return 'Hello world!'
+    return render_template('index.html', articles=articles)
+
+
+@app.route('/article/<int:article_id>')
+def article(article_id):
+    return render_template('article.html', article=articles[article_id - 1])
