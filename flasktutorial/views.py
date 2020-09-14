0e4b4dcfdfb55cd4636b9cbe5fcfcaa7aa7c3cc2
Magnus Book <magnus@bityoga.com>
2020-09-14 15:51:34 +0200

    Simple database implemented

--- flasktutorial/views.py
+++ flasktutorial/views.py
@@ -1,17 +1,15 @@
-import datetime
-from flasktutorial import app
 from flask import render_template
-
-articles = [{'id': 1, 'title': 'Something bad happened today!', 'text': 'Today was a bad day',
-             'date': datetime.date(1999, 4, 12)}, {'id': 2, 'title': 'Something good happened today!', 'text': 'Today was a good day',
-                                                   'date': datetime.date(2004, 6, 1)}]
+from flasktutorial import app, query_db
 
 
 @app.route('/')
 def index():
+    articles = query_db('SELECT rowid, * FROM article;')
     return render_template('index.html', articles=articles)
 
 
 @app.route('/article/<int:article_id>')
 def article(article_id):
-    return render_template('article.html', article=articles[article_id - 1])
+    article = query_db(
+        'SELECT * FROM article WHERE id=?;', (str(article_id)), True)
+    return render_template('article.html', article=article)
