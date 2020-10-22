e38b56316b5352bd8a593d5144c79aaed6f779f1
Magnus Book <magnus@bityoga.com>
2020-10-22 12:01:15 +0200

    Fixed loading sql file

--- flasktutorial/__init__.py
+++ flasktutorial/__init__.py
@@ -25,7 +25,7 @@ def init_db():
     with app.app_context():
         db = get_db()
         with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
+            db.cursor().execute(f.read())
         db.commit()
 
 
