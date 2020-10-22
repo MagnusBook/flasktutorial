01e3d2f03276dc776aaaab595bf2bbd8ca1b9026
Magnus Book <magnus@bityoga.com>
2020-10-22 11:05:12 +0200

    Added postgres

--- flasktutorial/__init__.py
+++ flasktutorial/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask, g
-import sqlite3
+import psycopg2
 app = Flask(__name__)
 
-DATABASE = 'database.db'
+DATABASE = 'postgresql:///DATABASE'
 
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    db.row_factory = sqlite3.Row
+        db = g._database = psycopg2.connect(DATABASE)
+    db.row_factory = psycopg2.Row
     return db
 
 
